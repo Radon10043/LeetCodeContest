e56e94669c243d3eee40d0625a33869c44562b9e
Radon10043 <772577952@qq.com>
2021-10-31 12:32:39 +0800

    Weekly contest 265

---
+++ WeeklyContest265.cpp
@@ -0,0 +1,113 @@
+/*
+ * @Author: Radon
+ * @Date: 2021-10-31 10:38:25
+ * @LastEditors: Radon
+ * @LastEditTime: 2021-10-31 12:30:54
+ * @Description: Hi, say something
+ */
+#include "headers/List.h"
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    /**
+     * @description: Accept
+     * @param {vector<int>} &nums
+     * @return {*}
+     */
+    int smallestEqual(vector<int> &nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (i % 10 == nums[i])
+                return i;
+        }
+        return -1;
+    }
+
+    /**
+     * @description: Accept
+     * @param {ListNode} *head
+     * @return {*}
+     */
+    vector<int> nodesBetweenCriticalPoints(ListNode *head) {
+        vector<int> vals;
+        int n = 0;
+        while (head) {
+            vals.emplace_back(head->val);
+            n++;
+            head = head->next;
+        }
+
+        if (n < 3)
+            return {-1, -1};
+        int first = -1, prev = -1, cur = -1, last = -1, minDistance = n;
+        for (int i = 1; i < n - 1; i++) {
+            if (vals[i] > vals[i + 1] && vals[i] > vals[i - 1] || vals[i] < vals[i + 1] && vals[i] < vals[i - 1]) { //临界点
+                last = i;
+                if (first == -1) {
+                    first = i;
+                    prev = i;
+                } else {
+                    cur = i;
+                    minDistance = min(minDistance, cur - prev);
+                    prev = cur;
+                }
+            }
+        }
+        if (cur == -1)
+            return {-1, -1};
+        return {minDistance, last - first};
+    }
+
+    /**
+     * @description: BFS+HashSet, 思路正确但Timeout
+     * @param {vector<int>} &nums
+     * @param {int} start
+     * @param {int} goal
+     * @return {*}
+     */
+    int minimumOperations(vector<int> &nums, int start, int goal) {
+        unordered_set<int> checked, prev, cur;
+        int cnt = 0;
+        cur.insert(start);
+        prev = cur;
+        while (!prev.empty()) {
+            cur.clear();
+            for (int data : prev) {
+                if (!(checked.insert(data).second))
+                    continue;
+                if (data < 0 || data > 1000)
+                    continue;
+                for (int num : nums) {
+                    if ((data + num == goal) || (data - num == goal) || ((data ^ num) == goal))
+                        return cnt + 1;
+                    if (data + num >= 0 && data + num <= 1000)
+                        cur.insert(data + num);
+                    if (data - num >= 0 && data - num <= 1000)
+                        cur.insert(data - num);
+                    if (data ^ num >= 0 && data ^ num <= 1000)
+                        cur.insert(data ^ num);
+                }
+            }
+            cnt++;
+            prev = cur;
+        }
+        return -1;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    Solution s;
+    vector<int> nums = {1, 3};
+    cout << s.minimumOperations(nums, 6, 4) << endl;
+    return 0;
+}
